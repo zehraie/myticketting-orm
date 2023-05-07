8980aa9f44d95ee71894642739872db117498e46
zehrairkicatal <zehraie197@gmail.com>
2023-05-07 10:31:27 +0100

    Initial commit

---
+++ src/main/java/com/cydeo/MytickettingOrmApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytickettingOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytickettingOrmApplication.class, args);
+    }
+
+}
